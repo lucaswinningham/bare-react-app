5964e206b0bd9469b174e018c2aec9bc3fd9c24d
Lucas Winningham <lucas.winningham@gmail.com>
2023-05-22 10:44:36 -0400

    Add initial eslint configuration file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,6 @@
 // @ts-check
 
-/* eslint-disable  @typescript-eslint/no-var-requires */
-
-const { defineConfig } = require('eslint-define-config');
-
-const eslintConfiguration = require('./eslint.configuration/eslint');
-const eslintConfiguration = require('./eslint.configuration/react');
-
-const configuration = defineConfig({
+const configuration = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -17,33 +10,22 @@ const configuration = defineConfig({
   },
   plugins: [
     '@typescript-eslint',
-    'unused-imports',
+    'react',
     'react-hooks',
-    'promise',
   ],
   extends: [
-    // 'eslint:recommended',
-    ...eslintConfiguration.extensions,
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'eslint-config-airbnb',
-    'eslint-config-prettier',
     'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:@typescript-eslint/strict',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:promise/recommended',
   ],
   settings: {
     react: {
       version: 'detect',
-    },
-    'import/resolver': {
-      typescript: true,
-      node: true,
     },
   },
   ignorePatterns: [
@@ -55,96 +37,9 @@ const configuration = defineConfig({
     '**/*.md',
     '**/*.yml',
   ],
+  // Rule customization
   rules: {
-    ...eslintConfiguration.rules,
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: [
-          '.tsx',
-        ],
-      },
-    ],
-    '@typescript-eslint/consistent-type-exports': [
-      'error',
-      {
-        fixMixedExportsWithInlineTypeSpecifier: true,
-      },
-    ],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        prefer: 'type-imports',
-        fixStyle: 'inline-type-imports',
-      },
-    ],
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    // Let unused-imports/no-unused-vars handle this
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
-    'import/consistent-type-specifier-style': [
-      'error',
-      'prefer-inline',
-    ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'import/no-default-export': 'error',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-      },
-    ],
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'sibling',
-          'parent',
-          'index',
-          'object',
-          'type',
-        ],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'import/prefer-default-export': 'off',
-    'promise/prefer-await-to-then': 'error',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'error',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
   },
-});
+};
 
 module.exports = configuration;
